99d09e6f144b0cf8d68499e72d806f9ca904d257
Similarity097 <v.garg1107@gmail.com>
2022-12-15 01:20:45 +0530

    updated gui

--- Pygame.py
+++
@@ -1,6 +0,0 @@
-import pygame
-#initalizeing pygame
-pygame.init()
-#creating the screen
-screen=pygame.display.set_mode((800,600))
-vsgvhagwj
--- main.py
+++ main.py
@@ -3,3 +3,17 @@ import pygame
 pygame.init()
 #creating the screen
 screen=pygame.display.set_mode((800,600))
+pygame.display.set_caption("Judgemental Typing")
+#icon for gui window
+icon = pygame.image.load('venta.jpg')
+pygame.display.set_icon(icon)
+screen.fill((0,0,0))
+pygame.display.update()
+
+running = True
+# keep game running till running is true
+while running:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+
