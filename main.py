1063d562c9746676abb6ab85e50bf8935d107b4e
Akshat Jain <Akshat17j@gmail.com>
2022-12-15 12:38:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ pygame.display.set_caption("Judgemental Typing")
 #icon for gui window
 icon = pygame.image.load('venta.jpg')
 pygame.display.set_icon(icon)
-screen.fill((0,0,0))
-pygame.display.update()
+
+
 
 running = True
 # keep game running till running is true
 while running:
+	screen.fill((0,0,0))
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+	pygame.display.update()
 
