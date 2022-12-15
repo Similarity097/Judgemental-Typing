e549e018b03771b8ef95dd6e1011b8a659032c1e
Similarity097 <v.garg1107@gmail.com>
2022-12-15 14:45:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,26 @@ pygame.display.set_caption("Judgemental Typing")
 #icon for gui window
 icon = pygame.image.load('venta.jpg')
 pygame.display.set_icon(icon)
-screen.fill((0,0,0))
 pygame.display.update()
+
+#basic colour codings
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
+
+#adding text to the gui window
+font = pygame.font.Font("freesansbold.ttf",32)
+text = font.render("Judemental Typing", True , green )
+textRect= text.get_rect()
+textRect.center = (400, 50)
 
 running = True
 # keep game running till running is true
 while running:
+	screen.fill((0,0,0))
+	screen.blit(text, textRect)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+	pygame.display.update()
 
